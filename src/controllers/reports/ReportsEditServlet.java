f269d6ed49d3293025adbdb3d1fe119b378cc0d1
kousuke.oshita <toranosukecow@gmail.com>
2019-08-17 00:25:52 +0900

    Add Report edit

--- src/controllers/reports/ReportsEditServlet.java
+++ src/controllers/reports/ReportsEditServlet.java
@@ -10,20 +10,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Employee;
 import models.Report;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class ReportsShowServlet
+ * Servlet implementation class ReportsEditServlet
  */
-@WebServlet("/reports/show")
-public class ReportsShowServlet extends HttpServlet {
+@WebServlet("/reports/edit")
+public class ReportsEditServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ReportsShowServlet() {
+    public ReportsEditServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -32,21 +33,25 @@ public class ReportsShowServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em =DBUtil.createEntityManager();
+        EntityManager em = DBUtil.createEntityManager();
 
         Report r = em.find(Report.class, Integer.parseInt(request.getParameter("id")));
 
         em.close();
 
-        request.setAttribute("report", r);
-        request.setAttribute("_token", request.getSession().getId());
+        Employee login_employee = (Employee)request.getSession().getAttribute("login_employee");
+        if(login_employee.getId() == r.getEmployee().getId()) {
+            request.setAttribute("report", r);
+            request.setAttribute("_token", request.getSession().getId());
+            request.getSession().setAttribute("report_id", r.getId());
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/show.jsp");
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/edit.jsp");
         rd.forward(request, response);
     }
 
 }
-
 
 
 
